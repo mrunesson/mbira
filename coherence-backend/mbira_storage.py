a2d08674ac11afdd0b75bf15430eb030282f91ef
Magnus Runesson <M.Runesson@gmail.com>
2011-04-25 18:21:28 +0200

    Initial structure of albums, tracks etc to browse exists.

--- coherence-backend/mbira_storage.py
+++ coherence-backend/mbira_storage.py
@@ -45,12 +45,27 @@ class TrackItem(BackendItem):
         return self.storage_id
 
 
+class ArtistContainer(Container):
+
+    def __init__(self, parent, artist):
+        Container.__init__(self, parent, artist[u'artist'][0])
+        self.artist=artist
+        
+
+    def populate_albums_for_artist(self):
+        pass
+
+
 class AlbumContainer(Container):
 
+    def __init__(self, parent, album):
+        Container.__init__(self, parent, album[u'album'][0])
+        self.album=album
+        
 
-    def populate_album(self):
+    def populate_tracks_for_album(self):
         pass
-    
+
 
 class MbiraStore(AbstractBackendStore):
 
@@ -66,17 +81,20 @@ class MbiraStore(AbstractBackendStore):
 
     def upnp_init(self):
         if self.server:
-            self.server.connection_manager_server.set_variable(0, 'SourceProtocolInfo',
-                        ['http-get:*:%s:*' % (TRACK_MIMETYPE)],
-                        default=True)
-            self.server.content_directory_server.set_variable(0, 'SystemUpdateID', self.update_id)
-
+            self.server.connection_manager_server.set_variable(0,
+                'SourceProtocolInfo',
+                ['http-get:*:%s:*' % (TRACK_MIMETYPE)],
+                default=True)
+            self.server.content_directory_server.set_variable(0,
+                'SystemUpdateID', self.update_id)
 
     def init_data(self):
         connection=pymongo.Connection()
         self.db=connection['mbira']
         self.init_container_structure()
         self.init_all_tracks()
+        self.init_all_artists()
+        self.init_all_albums()
         self.info("Init complete")
         self.init_completed()
 
@@ -86,22 +104,35 @@ class MbiraStore(AbstractBackendStore):
             item = TrackItem(t)
             external_id = t[u'_id']
             self.allTracksContainer.add_child(item, external_id = external_id)
+   
+    def init_all_artists(self):
+        artists=self.db.artists.find()
+        for a in artists:
+            item = ArtistContainer(self.get_root_item(), a)
+            external_id = a[u'_id']
+            self.artistsContainer.add_child(item, external_id = external_id)
 
-        
+    def init_all_albums(self):
+        albums=self.db.albums.find()
+        for a in albums:
+            item = AlbumContainer(self.get_root_item(), a)
+            external_id = a[u'_id']
+            self.albumsContainer.add_child(item, external_id = external_id)
+
 
     def init_container_structure(self):
         self.name = "Mbira"
         root_item = Container(None, self.name)
         self.set_root_item(root_item)
 
-        albumsContainer = Container(root_item, "Albums")
-        root_item.add_child(albumsContainer, external_id = "Albums")
+        self.albumsContainer = Container(root_item, "Albums")
+        root_item.add_child(self.albumsContainer, external_id = "Albums")
 
         self.allTracksContainer = Container(root_item, "All tracks")
         root_item.add_child(self.allTracksContainer, external_id = "AllTracks")
 
-        artistsContainer = Container(root_item, "Artists")
-        root_item.add_child(artistsContainer, external_id = "Artists")
+        self.artistsContainer = Container(root_item, "Artists")
+        root_item.add_child(self.artistsContainer, external_id = "Artists")
 
         genresContainer = Container(root_item, "Genres")
         root_item.add_child(genresContainer, external_id = "Genres")
